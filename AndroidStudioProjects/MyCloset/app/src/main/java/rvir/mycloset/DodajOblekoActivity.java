969049ec00beeaac96aa1e02618030784e8c0050
Eva <enykluns@gmail.com>
2018-06-05 14:53:28 +0200

    popravljano dodajane slike

--- AndroidStudioProjects/MyCloset/app/src/main/java/rvir/mycloset/DodajOblekoActivity.java
+++ AndroidStudioProjects/MyCloset/app/src/main/java/rvir/mycloset/DodajOblekoActivity.java
@@ -3,6 +3,9 @@ package rvir.mycloset;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +14,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class DodajOblekoActivity extends AppCompatActivity {
     private Button buttonDodajSliko;
     private ImageView imageView;
+    private Uri file;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,4 +49,38 @@ public class DodajOblekoActivity extends AppCompatActivity {
 
         startActivityForResult(intent, 100);
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                buttonDodajSliko.setEnabled(true);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(file);
+            }
+        }
+    }
+    //shranjevanje slike v pomnilnik
+    private static File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+    }
+
+
 }
